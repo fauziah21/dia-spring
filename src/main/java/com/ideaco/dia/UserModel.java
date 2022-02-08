daf0308390de69739f04cf22d0d9d548bb0ec614
Fauziah Aulia <fauziahaulia21@gmail.com>
2022-02-08 07:47:37 +0700

    menambahkan usermodel

---
+++ src/main/java/com/ideaco/dia/UserModel.java
@@ -0,0 +1,85 @@
+package com.ideaco.dia;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tab_user")
+public class UserModel {
+
+    //menyambungkan dengan table
+    @Id
+    //menandakan auto increment
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //mapping nama di table
+    @Column(name = "user_id")
+    private int userId;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "user_password")
+    private String userPassword;
+    @Column(name = "user_email")
+    private String userEmail;
+    @Column(name = "user_phone")
+    private String userPhone;
+    @Column(name = "user_address")
+    private String userAddress;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserResume() {
+        return userResume;
+    }
+
+    public void setUserResume(String userResume) {
+        this.userResume = userResume;
+    }
+
+    @Column(name = "user_resume")
+    private String userResume;
+}
